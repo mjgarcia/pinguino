80f9e114856c62a46cd9a2e186947747beb0b39e
Mauricio Garcia <mauriciojgv@gmail.com>
2018-07-16 00:34:22 -0700

    continue test extract attachments

--- pinguino/test/test_task_common.py
+++ pinguino/test/test_task_common.py
@@ -3,7 +3,22 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from tasks import task_common
+from message import Message
+
+MAKEFILE = 'Makefile'
+MODULE_FILE = 'hello_world_module.c'
+TEST_RESOURCES_DIR = 'resources'
+
+attachments_required = [MAKEFILE, MODULE_FILE]
+
+def get_test_attachments():
+    attachments = {}
+
+    with open(MAKEFILE) as f:
+        attachments[MAKEFILE] = f.read()
 
 def test_extract_attachments():
 
-    task_common.extract_attachments()
+    message = Message("test_message")
+
+    task_common.extract_attachments(attachments_required, message)
